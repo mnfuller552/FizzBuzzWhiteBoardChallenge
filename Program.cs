83fa6d752ab44318f64e49a532a3dd3abd2450d6
Matt Fuller <mnfuller552@gmail.com>
2023-07-14 12:43:30 -0400

    done

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FizzBuzzWhiteBoardChallenge
+{
+   public  class Program
+    {
+
+        
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a nunber");
+            var answer = Convert.ToInt32(Console.ReadLine());
+
+            static string FizzBuzz(int number)
+            {
+
+                var answer = "";
+
+                if (number % 3 == 0)
+                {
+                    answer = "Fizz";
+                }
+                if (number % 5 == 0)
+                {
+                    answer += "Buzz";
+                }
+                return answer;
+
+            }
+
+            Console.WriteLine(FizzBuzz(answer));
+
+
+        }
+
+
+    }
+
+
+
+}
+
